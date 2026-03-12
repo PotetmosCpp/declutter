ac0eb540bbb940b6f00ecfebb3bdf5b712da90ab
aet <alvaro.traelnes@gmail.com>
2026-03-12 20:00:48 +0100

    it shows unneeded and optional stuff now nice but holy repetivie code all over the place i dont care it works

--- crates/frontend/src/main.rs
+++ crates/frontend/src/main.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use eframe::{App, NativeOptions, egui::{self, CentralPanel, Image, ScrollArea, Vec2, include_image}};
+use eframe::{App, NativeOptions, egui::{self, CentralPanel, Image, RichText, ScrollArea, Vec2, include_image}};
 
 use backend::packages::Packages;
 
@@ -13,11 +13,11 @@ fn main() -> eframe::Result {
     packages.find_icon_paths().unwrap();
 
     //println!("Explicit: {:#?}", packages.explicit());
-    println!("Explicit:");
-    packages.explicit().iter()
-        .for_each(|package| println!("{}", package.name));
-    println!("Unneeded: {:#?}", packages.unneeded());
-    println!("Optional: {:#?}", packages.optional());
+    //println!("Explicit:");
+    //packages.explicit().iter()
+        //.for_each(|package| println!("{}", package.name));
+    //println!("Unneeded: {:#?}", packages.unneeded());
+    //println!("Optional: {:#?}", packages.optional());
 
     backend::remove_empty_dirs().unwrap();
 
@@ -31,8 +31,23 @@ fn main() -> eframe::Result {
             package.best_icon_path.as_deref().and_then(Path::to_str),
         ))
         .collect();
-
     explicit.sort_by_key(|x| x.icon_path.clone().is_none());
+
+    let mut unneeded: Vec<Package> = packages.unneeded().iter()
+        .map(|package| Package::new(
+            &package.name,
+            package.best_icon_path.as_deref().and_then(Path::to_str),
+        ))
+        .collect();
+    unneeded.sort_by_key(|x| x.icon_path.clone().is_none());
+
+    let mut optional: Vec<Package> = packages.optional().iter()
+        .map(|package| Package::new(
+            &package.name,
+            package.best_icon_path.as_deref().and_then(Path::to_str),
+        ))
+        .collect();
+    optional.sort_by_key(|x| x.icon_path.clone().is_none());
 
 
     eframe::run_native(
@@ -41,7 +56,9 @@ fn main() -> eframe::Result {
         Box::new(|cc| {
             egui_extras::install_image_loaders(&cc.egui_ctx);
             Ok(Box::new(Declutter {
-                explicit
+                explicit,
+                unneeded,
+                optional,
             }))
         }),
     )
@@ -63,24 +80,67 @@ impl Package {
 
 struct Declutter {
     explicit: Vec<Package>,
+    unneeded: Vec<Package>,
+    optional: Vec<Package>,
 }
 
 impl App for Declutter {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
             ScrollArea::vertical().show(ui, |ui| {
+                ui.label(RichText::new("Explicit").size(50.0));
                 ui.horizontal_wrapped(|ui| {
                     for package in &self.explicit {
                         ui.allocate_ui(Vec2::new(100.0, 150.0), |ui| {
                             ui.vertical_centered(|ui| {
                                 if let Some(icon_path) = &package.icon_path {
                                     ui.add(Image::new(
-                                        // test icon
                                         format!("file://{}", icon_path),
                                     ));
                                 } else {
                                     ui.add(Image::new(
-                                        // test icon
+                                        include_image!("/usr/share/icons/Cosmic/scalable/apps/application-default.svg"),
+                                    ));
+                                }
+
+                                ui.heading(&package.name);
+                            });
+                        });
+                    }
+                });
+
+                ui.label(RichText::new("Uneeded").size(50.0));
+                ui.horizontal_wrapped(|ui| {
+                    for package in &self.unneeded {
+                        ui.allocate_ui(Vec2::new(100.0, 150.0), |ui| {
+                            ui.vertical_centered(|ui| {
+                                if let Some(icon_path) = &package.icon_path {
+                                    ui.add(Image::new(
+                                        format!("file://{}", icon_path),
+                                    ));
+                                } else {
+                                    ui.add(Image::new(
+                                        include_image!("/usr/share/icons/Cosmic/scalable/apps/application-default.svg"),
+                                    ));
+                                }
+
+                                ui.heading(&package.name);
+                            });
+                        });
+                    }
+                });
+
+                ui.label(RichText::new("Optional").size(50.0));
+                ui.horizontal_wrapped(|ui| {
+                    for package in &self.optional {
+                        ui.allocate_ui(Vec2::new(100.0, 150.0), |ui| {
+                            ui.vertical_centered(|ui| {
+                                if let Some(icon_path) = &package.icon_path {
+                                    ui.add(Image::new(
+                                        format!("file://{}", icon_path),
+                                    ));
+                                } else {
+                                    ui.add(Image::new(
                                         include_image!("/usr/share/icons/Cosmic/scalable/apps/application-default.svg"),
                                     ));
                                 }
